5d156773ad9ae08628d879050f28c28e482c5f81
unknown <nguyendongminhtri29092014@gmail.com>
2021-06-30 16:38:39 +0700

    JWT DEMO

---
+++ src/main/java/com/example/demo/security/userprincal/UserDetailService.java
@@ -0,0 +1,22 @@
+package com.example.demo.security.userprincal;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+@Service
+public class UserDetailService implements UserDetailsService {
+    @Autowired
+    IUserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("User not found -> username or password"+username));
+        return UserPrinciple.build(user);
+    }
+}
